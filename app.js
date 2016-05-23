48ec8d1a22610364b124477d6e3d7a2ac1602316
cherry347 <StillLoriLeigh@gmail.com>
2016-05-23 22:14:51 +0000

    Simple new site drop

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+// Routes \\
+var indexController = require('./controllers/indexController')
+
+app.get('/', function(req, res){
+  res.sendFile('/html/index.html', {root : './public'})
+});
+
+
+// Creating Server and Listening for Connections \\
+var port = 80
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+});
